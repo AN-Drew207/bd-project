6638341871ef507ba14e701747b2370d855c2b0f
AN-Drew207 <andrescontrerasoviedo740@gmail.com>
2024-02-15 19:10:50 -0400

    Añadiendo inserciones de Conoce

--- PG Local.session.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Conoce (
-    nombre_personaje1 VARCHAR(50) NOT NULL,
-    nombre_personaje2 VARCHAR(50) NOT NULL,
-    tipo_relacion VARCHAR(70) NOT NULL,
-	PRIMARY KEY (nombre_personaje1,nombre_personaje2),
-    FOREIGN KEY (nombre_personaje1) REFERENCES Personaje(nombre),
-    FOREIGN KEY (nombre_personaje2) REFERENCES Personaje(nombre)
-);
