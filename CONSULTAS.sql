6f973d1be802b996dd2e3272244ce8dcd3b6f269
AN-Drew207 <andrescontrerasoviedo740@gmail.com>
2024-02-28 19:23:41 -0400

    Consulta 2 añadida

--- CONSULTAS.sql
+++ CONSULTAS.sql
@@ -1,18 +1,35 @@
+-- Consulta 1
 SELECT P.nombre, P.vision, P.region_proveniencia
 FROM Personaje P
 JOIN Region R ON R.nombre = P.region_proveniencia
 WHERE P.rareza = 4 AND P.vision <> R.elemento_origen
 ORDER BY P.nombre ASC, R.nombre ASC;
 
-SELECT nombre, Tipo, rareza
-FROM Arma
-WHERE ataque_base > 600;
+-- Consulta 2
+SELECT DISTINCT tipo
+FROM Arma a
+WHERE NOT EXISTS (SELECT 1 FROM Elemento e WHERE NOT EXISTS(
+    SELECT 1 FROM Personaje p WHERE 
+    p.tipo_arma = a.tipo
+    AND p.vision = e.nombre
+));
 
-SELECT *
-FROM RegionesInspiradas;
-
+-- Consulta 3
 SELECT nombre_personaje1
 FROM conoce 
 JOIN Ingiere I1 ON I1.nombre_personaje = nombre_personaje1
 JOIN Ingiere I2 ON I2.nombre_personaje = nombre_personaje2
 WHERE tipo_relacion = 'Enemistad' AND I1.nombre_comida = I2.nombre_comida;
+
+-- Consulta 4
+
+
+-- Consulta 5
+SELECT nombre, Tipo, rareza
+FROM Arma
+WHERE ataque_base > 600;
+
+-- Consulta 6
+SELECT *
+FROM RegionesInspiradas;
+
