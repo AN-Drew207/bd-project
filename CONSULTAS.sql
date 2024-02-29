562077328337dcb02173a3a9c6b6c9ccf5263ef2
AN-Drew207 <andrescontrerasoviedo740@gmail.com>
2024-02-29 14:34:31 -0400

    Añadiendo consulta 8

--- CONSULTAS.sql
+++ CONSULTAS.sql
@@ -61,23 +61,60 @@ OR nombre IN
         AND magnitudSegundoEfecto > 10));
 
 -- Consulta 8
--- Listar los nombres de los personajes que tienen una amistad con al menos un personaje con efecto secundario %Daño Critico y que su magnitud sea mayor a 10
--- Esta consulta fue elaborada ya que la tabla efectos no es tan usada y aprovechando la relacion conoce entre personajes, podemos encontrar
--- los personajes con amigos que tengan el efecto secundario '%Daño Critico' mayor que 10
-SELECT nombre 
-FROM Personaje
-WHERE nombre IN 
-    (SELECT nombrePersonaje1 FROM conoce
-    WHERE tipoRelacion = 'Amistad'
-    AND nombrePersonaje2 IN
-        (SELECT nombre FROM Personaje 
-        WHERE efectoSecundario = (SELECT id FROM Efecto WHERE nombre = '%Daño Critico') 
-        AND magnitudSegundoEfecto > 10)) 
-OR nombre IN 
-    (SELECT nombrePersonaje2 FROM conoce
-    WHERE tipoRelacion = 'Amistad'
-    AND nombrePersonaje1 IN
-        (SELECT nombre FROM Personaje 
-        WHERE efectoSecundario = (SELECT id FROM Efecto WHERE nombre = '%Daño Critico')
-        AND magnitudSegundoEfecto > 10));
+-- Mostrar la cantidad de enemigos que existen en cada abismo abisal ordenados por ID
+-- Se realizó esta consulta porque la tabla enemigo no es tan usada en la consulta y ademas es
+-- necesario saber el total de enemigos por abismo abisal como buen recurso de información para
+-- los jugadores
 
+SELECT COUNT(E.nombre), I.idabismoabisal 
+FROM Enemigo E
+JOIN Incluye I ON E.nombre = I.nombreEnemigo
+JOIN AbismoAbisal AA ON E.nombre = I.nombreEnemigo
+WHERE I.idabismoabisal = AA.id
+GROUP BY I.idabismoabisal
+ORDER BY I.idabismoabisal;
+
+
+
+-- Función TierSSS 
+CREATE FUNCTION TierSSS(elemento VARCHAR)
+RETURNS VOID AS $$
+DECLARE
+    personaje VARCHAR(70);
+    ataque_promedio FLOAT;
+BEGIN
+    -- Verificar si el elemento existe en la tabla Elemento y tiene al menos un poseedor
+    IF EXISTS (
+        SELECT 1
+        FROM Elemento E
+        JOIN Personaje P ON E.nombre = P.vision
+        WHERE E.nombre = elemento
+    )
+    THEN
+        -- Obtener el mejor personaje del elemento
+        SELECT P.nombre,
+               (P.AtaqueBase + A.AtaqueBase) + 
+               (P.AtaqueBase + A.AtaqueBase) * (SELECT bonoATQ FROM Habilidad WHERE nombre = P.habilidadElemental) +
+               (P.AtaqueBase + A.AtaqueBase) * (SELECT bonoATQ FROM Habilidad WHERE nombre = P.habilidadDefinitiva)
+        INTO personaje, ataque_promedio
+        FROM Elemento E
+        JOIN Personaje P ON E.nombre = P.vision
+        JOIN Arma A ON P.nombreArma = A.nombre
+        WHERE E.nombre = elemento
+        ORDER BY ataque_promedio DESC
+        LIMIT 1;
+        -- Retornar el mensaje con el ataque promedio
+        RAISE NOTICE 'El mejor personaje del elemento % es %, teniendo un ataque promedio de % puntos.', elemento, personaje, ataque_promedio;
+    ELSE
+        -- Elemento no encontrado
+        RAISE NOTICE 'El elemento ingresado no corresponde a ningún elemento de la tabla Elemento';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT TierSSS('Electro');
+SELECT TierSSS('Hydro');
+SELECT TierSSS('Pairo');
+SELECT TierSSS('Geo');
+SELECT TierSSS('Dendro');
